4eed16aee29333cfd423d0e36440e34fe45af046
ONYEDIKA ILODIBE <85747585+chrisilodibe@users.noreply.github.com>
2023-11-09 09:51:25 +0100

    commit

---
+++ frontend/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./Global.scss";
+import ThemeContextProvider from './context/theme.context';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <ThemeContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeContextProvider>
+);
+
---
+++ frontend/src/pages/home/Home.page.tsx
@@ -0,0 +1,26 @@
+import './home.scss'
+
+const Home = () => {
+    return (
+        <div className='content home'>
+            <h3>Welcome To Website</h3>
+            <br />
+            <br />
+            <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quidem id architecto corporis quam odit dolore quasi soluta quos.
+                Iure distinctio quibusdam in quisquam illum quas libero magni eum
+                cum adipisci dignissimos temporibus esse deserunt rerum modi dicta,
+                deleniti enim fugit excepturi impedit eligendi culpa nihil ea.
+                Laborum, expedita ipsum? Aperiam temporibus magni totam molestiae itaque,
+                inventore unde repudiandae eum provident dolorem error incidunt necessitatibus
+                odio labore laudantium ex facilis dolore nobis quae. Veritatis, adipisci minus.
+                Dignissimos quod autem ullam doloribus cumque nostrum fuga officiis,
+                adipisci soluta temporibus harum qui, cupiditate laboriosam facilis inventore,
+                sapiente fugit? Impedit ullam deleniti repellat atque?
+            </span>
+        </div>
+    )
+}
+
+export default Home
