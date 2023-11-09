e39bdd122f9629add9bd7d4da0d658313c87dd55
ONYEDIKA ILODIBE <85747585+chrisilodibe@users.noreply.github.com>
2023-11-09 15:07:29 +0100

    commit

--- frontend/src/pages/home/Home.page.tsx
+++ frontend/src/pages/home/Home.page.tsx
@@ -1,26 +1,30 @@
-import './home.scss'
 
-const Home = () => {
+import React, { useState, ChangeEvent } from 'react';
+import './home.scss';
+
+const Home: React.FC = () => {
+    const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files && e.target.files[0];
+
+        if (file) {
+            setSelectedImage(file);
+        }
+    };
+
     return (
         <div className='content home'>
             <h3>Welcome To Website</h3>
             <br />
             <br />
             <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quidem id architecto corporis quam odit dolore quasi soluta quos.
-                Iure distinctio quibusdam in quisquam illum quas libero magni eum
-                cum adipisci dignissimos temporibus esse deserunt rerum modi dicta,
-                deleniti enim fugit excepturi impedit eligendi culpa nihil ea.
-                Laborum, expedita ipsum? Aperiam temporibus magni totam molestiae itaque,
-                inventore unde repudiandae eum provident dolorem error incidunt necessitatibus
-                odio labore laudantium ex facilis dolore nobis quae. Veritatis, adipisci minus.
-                Dignissimos quod autem ullam doloribus cumque nostrum fuga officiis,
-                adipisci soluta temporibus harum qui, cupiditate laboriosam facilis inventore,
-                sapiente fugit? Impedit ullam deleniti repellat atque?
+                <h1>Welcome To ResumeVault, we are glad to have you</h1>
+                <h2>Please make sure you submit a weighty Resume to land you a mouth-watering Job</h2>
+                
             </span>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
